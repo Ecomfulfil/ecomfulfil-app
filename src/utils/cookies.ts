b0dadf6a0f2246e91dca0735295de35644b3b1dd
Zubair Sheikh <zubair@xertk.com>
2024-06-13 21:20:44 +0500

    fix: cookies issue

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -3,23 +3,18 @@ import { setCookie, getCookie, deleteCookie } from 'cookies-next';
 // Function to set a cookie
 export const setSessionCookie = (key: any, value: any) => {
   setCookie(key, value, {
-    domain: process.env.NEXT_PUBLIC_DOMAIN || 'localhost',
     path: '/',
     maxAge: 60 * 60 * 24 * 7, // 1 week
-    secure: process.env.NODE_ENV === 'production',
+    secure: true,
   });
 };
 
 // Function to get a cookie
 export const getSessionCookie = (key: any) => {
-  return getCookie(key, {
-    domain: process.env.NEXT_PUBLIC_DOMAIN || 'localhost',
-  });
+  return getCookie(key);
 };
 
 // Function to delete a cookie
 export const deleteSessionCookie = (key: any) => {
-  deleteCookie(key, {
-    domain: process.env.NEXT_PUBLIC_DOMAIN || 'localhost',
-  });
+  deleteCookie(key);
 };
